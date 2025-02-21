c8d3e110a0c552bdcd5b0fb061112bb6227e2c18
Maksiks <maksiksking@gmail.com>
2025-02-21 21:36:55 +0200

    tice: conveyor belt edition

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,18 @@
 <script setup>
-import { ref, onMounted } from "vue";
-import { info } from "@tauri-apps/plugin-log";
+import {ref, onMounted} from "vue";
+import {info} from "@tauri-apps/plugin-log";
 
 // conf
 
-const gridSize = ref(50);
+const scale = 1;
 
 // conf
 
+const translateX = ref(0)
+const translateY = ref(0)
+const iniX = ref(0);
+const iniY = ref(0);
+const isPanning = ref(false);
 
 const canvasElemRef = ref(null);
 
@@ -18,26 +23,44 @@ function test() {
 
   const gridSize = ref(50);
 
-  info((window.innerWidth/50).toString());
+  info((window.innerWidth / 50).toString());
   info("a");
 }
 
-async function drawGrid(ctx, size) {
-  for (let i = 0; i < window.innerWidth/size; i++) {
-    ctx.moveTo(0, i * size);
-    ctx.lineTo(window.innerWidth, i * size);
+async function drawGrid() {
+  const canvasElement = canvasElemRef.value;
+  const ctx = canvasElement.getContext("2d");
+  const size = 50 * scale;
 
-    ctx.moveTo(i * size, 0);
-    ctx.lineTo(i * size, window.innerHeight);
+  // this is smart af, would never have come up with this myself
+  const offsetX = translateX.value % size;
+  const offsetY = translateY.value % size;
+
+  const { width, height } = canvasElement;
+
+  for (let x = offsetX; x < width; x += size) {
+    ctx.moveTo(x, 0);
+    ctx.lineTo(x, height);
+  }
+  for (let y = offsetY; y < height / size; y += size) {
+    ctx.moveTo(0, y);
+    ctx.lineTo(width, y);
   }
   ctx.strokeStyle = "white"
+  ctx.lineWidth = 1;
   ctx.stroke()
 }
 
-function onMouseDown(e) {
-  if (e.button === 1) {
-    info("midlle button my lord")
+function startPanning(e) {
+  if (!e.button === 1) {
+    return
   }
+  isPanning.value = true;
+
+  info("middle button my lord")
+
+  iniX.value = e.clientX - translateX.value;
+  iniY.value = e.clientY - translateY.value;
 }
 
 function renderGrid() {
@@ -45,15 +68,24 @@ function renderGrid() {
 
   canvasElement.width = window.innerWidth;
   canvasElement.height = window.innerHeight;
-  const ctx = canvasElement.getContext("2d");
 
-  drawGrid(ctx, gridSize.value);
+  drawGrid();
+  if (isPanning.value) {
+    pan()
+  }
+}
+
+function pan() {
+  translateX.value = translateX.value - iniX.value;
+  translateY.value = translateY.value - iniY.value;
+  drawGrid()
 }
 
 function onMouseUp(e) {
   if (e.button === 1) {
     info("middle button ran away")
   }
+  isPanning.value = false;
 }
 
 onMounted(() => {
@@ -63,7 +95,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <div @mousedown="onMouseDown" @mousemove="renderGrid" @mouseup="onMouseUp" class="the-wrapper">
+  <div @mousedown="startPanning" @mousemove="renderGrid" @mouseup="onMouseUp" class="the-wrapper">
     <canvas ref="canvasElemRef" class="the-canvas"></canvas>
     <button class="test-button" @click="test">test</button>
   </div>
